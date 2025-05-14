a9f88806430cbb4fba5894ab8ba0f214e84fa269
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-05-15 04:25:27 +0900

    fix(nav): 사이드바 내의 캐릭터 페이지 링크 수정 (#9, #26)

--- src/ui/Nav/ui/Sidebar/Sidebar.tsx
+++ src/ui/Nav/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
+import Link from 'next/link'
 import { FC } from 'react'
 import { container } from './sidebar.css'
-import Link from 'next/link'
 
 interface Props {
   isOpen: boolean
@@ -9,7 +9,7 @@ interface Props {
 const Sidebar: FC<Props> = ({ isOpen }) => {
   return (
     <aside className={container({ status: isOpen ? 'open' : 'close' })}>
-      <Link href="/introduction">캐릭터 소개 페이지로 이동</Link>
+      <Link href="/character">캐릭터 소개 페이지로 이동</Link>
       <div>외 기타 등등 추가 예정</div>
     </aside>
   )
