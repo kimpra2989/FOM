86573d78739a80e254d2ab68339524a254f08521
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-05-15 04:32:56 +0900

    feat(character): CanvasWrapper 컴포넌트 구현 (#27, #28)

---
+++ src/ui/canvas/Canvas/Canvas.tsx
@@ -0,0 +1,36 @@
+import { Canvas } from '@react-three/fiber'
+import { FC, ReactNode } from 'react'
+
+interface Props {
+  width?: number
+  height?: number
+  cameraPos?: [number, number, number]
+  children?: ReactNode
+  className?: string
+}
+
+const CanvasWrapper: FC<Props> = ({
+  width = window.innerWidth,
+  height = window.innerHeight,
+  cameraPos = [0, 0, 0.15],
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={className}
+      style={{ width: `${width}px`, height: `${height - 60}px` }}
+    >
+      <Canvas
+        camera={{
+          position: cameraPos,
+          aspect: width / (height - 60),
+        }}
+      >
+        {children}
+      </Canvas>
+    </div>
+  )
+}
+
+export default CanvasWrapper
