b384680a0096b48f103e83858f1f11c5a49e3736
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-05-25 17:30:29 +0900

    feat(shape): 도형을 위한 base 컴포넌트 (이미지, 텍스트) 추가 (#23, #32)

--- src/ui/canvas/index.ts
+++ src/ui/canvas/index.ts
@@ -1,3 +1,5 @@
 export * from './Canvas'
 export * from './character'
 export * from './scene'
+export * from './shapes'
+
---
+++ src/ui/canvas/shapes/constants.ts
@@ -0,0 +1,17 @@
+export const Shape = {
+  text: {
+    fontSize: 16,
+    lineHeight: 1.3,
+    fontStyle: 'bold',
+    fill: 'white',
+    text: 'CREATE\nFOM',
+    align: 'center',
+    verticalAlign: 'middle',
+  },
+  circle: {
+    r: 65,
+  },
+  square: {
+    len : 116
+  }
+}
