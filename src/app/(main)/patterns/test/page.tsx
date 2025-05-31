a439370a4982aa7fe11282af6d08b5013451f0c6
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-05-31 21:20:27 +0900

    feat(pattern): 경험 패턴 페이지에 경험 3 추가 (#33)

--- src/app/(main)/patterns/test/page.tsx
+++ src/app/(main)/patterns/test/page.tsx
@@ -12,6 +12,9 @@ export default function Test2Page() {
       <Link href={'./test/exp21'}>경험 2 - 1</Link>
       <Link href={'./test/exp22'}>경험 2 - 2</Link>
       <Link href={'./test/exp23'}>경험 2 - 3</Link>
+      <Link href={'./test/exp31'}>경험 3 - 1</Link>
+      <Link href={'./test/exp32'}>경험 3 - 2</Link>
+      <Link href={'./test/exp33'}>경험 3 - 3</Link>
     </div>
   )
 }
