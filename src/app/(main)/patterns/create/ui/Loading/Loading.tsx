a6616468885e904ebb074213e7a3b9d8cc8492c3
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-06-04 10:44:02 +0900

    fix(create): 설문 관련 흐름 수정

--- src/app/(main)/patterns/create/ui/Loading/Loading.tsx
+++ src/app/(main)/patterns/create/ui/Loading/Loading.tsx
@@ -2,7 +2,9 @@ import { container } from './loading.css'
 
 const LoadingStep = () => {
   return (
-    <div className={container}>You의 accident 방식이 조합되는 중입니다.</div>
+    <div className={container}>
+      <video src={'/LoadingVideo.webm'} autoPlay muted />
+    </div>
   )
 }
 
--- src/app/(main)/patterns/create/ui/Result/Result.tsx
+++ src/app/(main)/patterns/create/ui/Result/Result.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { getResultPatternData } from './fn'
-import { buttons, container } from './result.css'
+import { container } from './result.css'
 import BasePattern from './ui/BasePattern'
 
 interface Props {
@@ -8,30 +8,29 @@ interface Props {
 }
 
 const ResultStep: FC<Props> = ({ answer }) => {
-  const ex = [2, 2, 125, 591, 591, 1, 2, 154, 658, 568, 1, 1, 175, 587, 531]
+  // const ex = [2, 2, 125, 591, 591, 1, 2, 154, 658, 568, 1, 1, 175, 587, 531]
   // const ex2 = [2, 0, 100, 520, 156, 2, 2, 70, 636, 651, 2, 2, 240, 120, 606]
 
-  const { colors, shapes } = getResultPatternData(ex)
+  const { colors, shapes } = getResultPatternData(answer)
   // const { colors, shapes } = getResultPatternData(answer)
 
   return (
     <>
-      <div className={buttons}>
+      {/* <div className={buttons}>
         <button onClick={() => window.print()}> 패턴 출력</button>
-      </div>
+      </div> */}
       <div className={container}>
         <div>
-          {colors.map((color) =>
-            color.color === 'black' ? (
-              <div key={color.color}>black</div>
-            ) : (
-              <BasePattern
-                color={color.color}
-                hex={color.hex}
-                shape={shapes[color.color]}
-                key={color.color}
-              />
-            )
+          {colors.map(
+            (color) =>
+              color.color !== 'black' && (
+                <BasePattern
+                  color={color.color}
+                  hex={color.hex}
+                  shape={shapes[color.color]}
+                  key={color.color}
+                />
+              )
           )}
         </div>
       </div>
